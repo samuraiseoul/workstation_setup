c2c7c92f6fe0caf20e277acc2df4fba4672f7f26
samuraiseoul <scottlavigne28@gmail.com>
2019-05-04 16:40:55 -0500

    fix bash_include

--- bash/bash_include.sh
+++ bash/bash_include.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-source "$PWD/bash/partials/prompt.sh"
-source "$PWD/bash/partials/variables.sh"
-source "$PWD/bash/partials/dir_aliases.sh"
-source "$PWD/bash/partials/aliases.sh"
-source "$PWD/bash/partials/functions.sh"
+source "$PWD/.bash/partials/prompt.sh"
+source "$PWD/.bash/partials/variables.sh"
+source "$PWD/.bash/partials/dir_aliases.sh"
+source "$PWD/.bash/partials/aliases.sh"
+source "$PWD/.bash/partials/functions.sh"
