9c7b0f2ac27f9db18828f72c9b17766fc2c41748
samuraiseoul <scottlavigne28@gmail.com>
2019-05-07 16:22:11 +0000

    remove weird thing from repo

--- playbooks/lookup('env','HOME')/.bashss/bash/application_configs/git.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git config --global user.email ${EMAIL}
-git config --global user.name ${NAME}
-git config --global user.username ${USERNAME}
-git config --global core.editor ${EDITOR}
-git config commit.gpgsign true
--- playbooks/lookup('env','HOME')/.bashss/bash/bash_include.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source "$PWD/bash/partials/prompt.sh"
-source "$PWD/bash/partials/variables.sh"
-source "$PWD/bash/partials/dir_aliases.sh"
-source "$PWD/bash/partials/aliases.sh"
-source "$PWD/bash/partials/functions.sh"
