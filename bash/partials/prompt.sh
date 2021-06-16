22f4e1cd5292b0e31ea5cc76f191c67927d62cb9
Sophie The Lionhart <scottlavigne28@gmail.com>
2021-06-16 12:00:12 -0500

    Update prompt.sh

--- bash/partials/prompt.sh
+++ bash/partials/prompt.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 
-# from http://ezprompt.net/
 
-# get current branch in git repo
-function parse_git_branch() {
-	BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
-	if [ ! "${BRANCH}" == "" ]
-	then
-		echo -e "\e[1m\e[31m⎇ \e[0m\e[32m${BRANCH}"
-	else
-		echo ""
-	fi
-}
+CURRENT_DIR="\[\e[34m\]\W"
+BRANCH="\$(__git_ps1 '\[\e[1m\e[31m\] ⎇  \[\e[0m\e[32m\]%s')"
+PROMPT_SYMBOL="\[\e[1m\e[31m\]➤"
 
-CURRENT_DIR="\e[34m\w"
-BRANCH="\$(parse_git_branch)"
-PROMPT_SYMBOL="\e[1m\e[31m➤"
-
-export PS1="$CURRENT_DIR$BRANCH $PROMPT_SYMBOL \e[0m"
+export PS1="$CURRENT_DIR$BRANCH $PROMPT_SYMBOL \[\e[0m\]"
