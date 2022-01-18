546650db70b8503872a9598999648f26f546776f
Sophie The Lionhart <sophielionhart@gmail.com>
2022-01-18 11:53:25 -0600

    Update prompt.sh

--- bash/partials/prompt.sh
+++ bash/partials/prompt.sh
@@ -5,4 +5,5 @@ CURRENT_DIR="\[\e[34m\]\W"
 BRANCH="\$(__git_ps1 '\[\e[1m\e[31m\] ⎇  \[\e[0m\e[32m\]%s')"
 PROMPT_SYMBOL="\[\e[1m\e[31m\]➤"
 
-export PS1="$CURRENT_DIR$BRANCH $PROMPT_SYMBOL \[\e[0m\]"
+#export PS1="$CURRENT_DIR$BRANCH $PROMPT_SYMBOL \[\e[0m\]"
+export PS1='\[\e[34m\]\W $(__git_ps1 "\[\e[1m\e[31m: \]\[\e[0m\e[32m\]%s") \[\e[1m\e[31m\]> \[\e[0m\]'
